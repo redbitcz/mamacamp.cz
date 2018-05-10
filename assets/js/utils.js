ff45c7d220d9eb4276fa510baa1d18b8efca2c85
Jakub Bouček <pan@jakubboucek.cz>
2018-05-10 09:29:07 +0200

    First version (from Punkcamp)

---
+++ assets/js/utils.js
@@ -0,0 +1,116 @@
+<html>
+ <head>
+  <title>punkcamp.cz - error 404</title>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+  <style type="text/css">
+  <!--
+	body {
+		position: 			absolute;
+		top: 				0px;
+		left: 				0px;
+		right: 				0px;
+		width:				100%;
+		height:				100%;
+		padding: 			0px;
+		margin: 			0px;
+		border: 			0px;
+		overflow:			auto;
+		color:				#193738;
+		background-color: 		#F5F5F5;
+	}
+
+	/*********************************** LINKS ************************************/
+	a.link, a.link:link, a.link:visited {
+		color:			#3bc492;
+		text-decoration:	none;
+		font-weight:		bold;
+	}
+
+	a.link:hover, a.link:active, a.link:focus {
+		text-decoration:	underline;
+		font-size: 		18px;
+	}
+
+
+	p {
+		font-family:		sans-serif, arial, helvetica ;
+		font-weight:		100;
+		margin-top:		20px;
+		margin-bottom:		5px;
+		text-align:		center;
+		font-size: 		18px;
+	}
+	h2 {
+		font-family:		sans-serif, arial, helvetica ;
+		font-weight:		400;
+		margin-top:		20px;
+		margin-bottom:		5px;
+		text-align:		center;
+		font-size: 		32px;
+	}
+	hr {
+	    height: 1px; 
+	    background-color: #E4E4E4;
+    	    border:none
+        }
+	.banner {
+	    margin-top: 60px;
+	    border-color: #ccc;
+	}
+	.banner img{
+	    border-color: #E4E4E4;
+	}
+
+	/*********************************** DIVs ************************************/
+
+	#header {
+		color:			#193738;
+		width:			100%;
+		height:			90px;
+		white-space: 		nowrap;
+	}
+	#header img {
+	    max-height:			85px;
+	    max-width:			auto;
+	    margin-top:		        5px;
+	    padding-left:		30px;
+	    float: left;
+	}
+
+	#content {
+		background-color:	#F5F5F5;
+		color:			#193738;
+		width:			80%;
+		margin-top:		50px;
+		margin-left:		auto;
+		margin-right:		auto;
+		z-index:		1;
+	}
+
+	#center {
+		text-align:		center;
+	}
+
+	#bottom {
+		position:		absolute;
+		bottom:			0px;
+		right:			10px;
+		text-align:		right;
+		z-index:		0;
+	}
+   //-->
+  </style>
+ </head>
+ <body>
+  <div id="header"><img src="/errors/lm_logo.gif"  alt="eBola.cz"/></div>
+  <hr>
+  <div id="center">
+   <div id="content">
+    <h2>punkcamp.cz Error 404!</h2>
+    <p>Document Not Found</p>
+    <p class="banner"><img src="http://www.ebola.cz/img/ebola-hosting-320x250.gif" border=1></p>
+   </div>
+  </div>
+
+ </body>
+</html>
